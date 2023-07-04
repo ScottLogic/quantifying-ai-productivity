0e5be2afe4fc45b77a26e66c1edc35482f05d91c
pgraham-sl <pgraham@scottlogic.com>
2023-07-04 17:28:02 +0100

    Refactored logic to skeleton version of code.

--- src/java/src/main/java/com/scottlogic/quantifying/ai/services/TaskListService.java
+++ src/java/src/main/java/com/scottlogic/quantifying/ai/services/TaskListService.java
@@ -33,59 +33,8 @@ public class TaskListService {
         }
     }
 
-    public List<ToDoTask> getToDoTaskList(Optional<Boolean> completed) {
-        if (completed.isPresent()) {
-            return toDoTaskList.stream()
-                    .filter(t -> t.isComplete() == completed.get())
-                    .collect(Collectors.toList());
-        }
+    public List<ToDoTask> getToDoTaskList() {
         return toDoTaskList;
-    }
-
-    /**
-     * Returns the ToDoTask with the given uuid.
-     * If no task is found with the given uuid then the ToDoTask.UNKNOWN_TASK is returned.
-     * @param uuid
-     * @return The ToDoTask with the given uuid, if found, otherwise ToDoTask.UNKNOWN_TASK is returned.
-     */
-    public ToDoTask getToDoTaskById(UUID uuid) {
-        return toDoTaskList.stream()
-                .filter(t -> t.getUuid().equals(uuid))
-                .findFirst().orElse(ToDoTask.UNKNOWN_TASK);
-    }
-
-    /**
-     * Add the given ToDoTask to the list of tasks.
-     * @param name The name for the new task.
-     * @param description The description for the new task.
-     */
-    public AddTaskResponse addTask(String name, String description) {
-        ToDoTask newTask = new ToDoTask(name, description);
-        toDoTaskList.add(newTask);
-        return new AddTaskResponse(newTask.getUuid(), "Task " + newTask.getName() + " added successfully.");
-    }
-
-    /**
-     * Marks the ToDoTask with the given uuid as complete.
-     * If no task is found with the given uuid then "Task not found." is returned.
-     * If the task is found and is already marked complete then "Task already marked complete." is returned.
-     * @param uuid The uuid of the task to be marked complete.
-     */
-    public CompletionResponse completeToDoTask(UUID uuid) {
-        ToDoTask toDoTask = getToDoTaskById(uuid);
-
-        if (Objects.nonNull(toDoTask)) {
-            if (toDoTask == ToDoTask.UNKNOWN_TASK) {
-                return new CompletionResponse(false, "Task not found.");
-            } else if (toDoTask.isComplete()) {
-                return new CompletionResponse(false, "Task already marked complete.");
-            }
-
-            toDoTask.setComplete(true);
-            return new CompletionResponse(true, "This task has now been completed.");
-        }
-
-        return new CompletionResponse(false, "Unexpected error.");
     }
 
 }
