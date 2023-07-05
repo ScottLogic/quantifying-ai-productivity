90f2fe392e8675234e7feb158ac04d7327b2dd7f
pgraham-sl <pgraham@scottlogic.com>
2023-07-05 11:33:19 +0100

    Skeleton code for Java and JavaScript.

--- src/java/src/main/java/com/scottlogic/quantifying/ai/controllers/TaskListController.java
+++ src/java/src/main/java/com/scottlogic/quantifying/ai/controllers/TaskListController.java
@@ -14,33 +14,15 @@ import java.util.*;
 @RequestMapping("todo")
 public class TaskListController {
 
-    @Autowired
-    TaskListService taskListService;
+    private final TaskListService taskListService;
+
+    public TaskListController(TaskListService taskListService) {
+        this.taskListService = taskListService;
+    }
 
     @GetMapping("")
-    public List<ToDoTask> getAllTasks(@RequestParam Optional<Boolean> complete) {
-        return taskListService.getToDoTaskList(complete);
-    }
-
-    @GetMapping("/{uuid}")
-    public ToDoTask getTaskById(@PathVariable UUID uuid) {
-        return taskListService.getToDoTaskById(uuid);
-    }
-
-    @PutMapping("/completed/{uuid}")
-    public ResponseEntity<CompletionResponse> markTaskAsCompleted(@PathVariable UUID uuid) {
-        CompletionResponse response = taskListService.completeToDoTask(uuid);
-
-        if (response.isSuccess()) {
-            return ResponseEntity.ok().body(response);
-        }
-
-        return ResponseEntity.badRequest().body(response);
-    }
-
-    @PostMapping("/addTask")
-    public ResponseEntity<AddTaskResponse> addTask(@RequestParam() String name, @RequestParam String description) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(taskListService.addTask(name, description));
+    public List<ToDoTask> getAllTasks() {
+        return taskListService.getToDoTaskList();
     }
 
 }
