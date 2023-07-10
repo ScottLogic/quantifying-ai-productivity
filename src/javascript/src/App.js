77d43a2aa598a9286c2f3172a360877c106b55e3
pgraham-sl <pgraham@scottlogic.com>
2023-07-10 14:28:04 +0100

    Changed the JS code to load the static data.

--- src/javascript/src/App.js
+++ src/javascript/src/App.js
@@ -1,44 +1,41 @@
 const express = require('express');
 const { v4: uuidv4, validate: uuidValidate} = require('uuid');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 
-const tasks = [
-    {
-      uuid: "f360ba09-4682-448b-b32f-0a9e538502fa",
-      name: "Walk the dog",
-      description: "Walk the dog for forty five minutes",
-      created: new Date("2023-06-23T09:30:00Z").toISOString(),
-      completed: null,
-      complete: false
-    },
-    {
-    uuid: "fd5ff9df-f194-4c6e-966a-71b38f95e14f",
-    name: "Mow the lawn",
-    description: "Mow the lawn in the back garden",
-    created: new Date("2023-06-23T09:00:00Z").toISOString(),
-    completed: null,
-    complete: false
-    },
-    {
-    uuid: "5c3ec8bc-6099-4cd5-b6da-8e2956db3a34",
-    name: "Test generative AI",
-    description: "Use generative AI technology to write a simple web service",
-    created: new Date("2023-06-23T09:00:00Z").toISOString(),
-    completed: null,
-    complete: false
-    }
- ];
+const tasksFilePath = path.join(__dirname, '../../static_data', 'ToDoList.json');
 
- const UNKNOWN_TASK = {
+let tasks = [];
+
+const UNKNOWN_TASK = {
     uuid: "00000000-0000-0000-0000-000000000000", 
     name: "Unknown Task",
     description: "Unknown Task",
     created: new Date(0).toISOString(),
     completed: null,
     complete: false
- }
+}
+
+// Load tasks from the JSON file
+const loadTasksFromFile = () => {
+    fs.readFile(tasksFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading tasks file:', err);
+            return;
+        }
+        try {
+            tasks = JSON.parse(data);
+        } catch (error) {
+            console.error('Error parsing tasks JSON:', error);
+        }
+    });
+};
+
+// Load tasks from the file when the server starts
+loadTasksFromFile();
 
 // Get all tasks
 app.get('/todo', (req, res) => {
