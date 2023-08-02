05413291117b79c338ee27e9ddab522d17343e3a
Ged Smith <gsmith@scottlogic.com>
2023-08-02 16:45:06 +0100

    Completed POST Task 2

--- test/cypress/e2e/api-tests/postToDoAddTaskMissingNameAndDescription.cy.js
+++ test/cypress/e2e/api-tests/postToDoAddTaskMissingNameAndDescription.cy.js
@@ -1,3 +1,14 @@
+//chatGpt request:
+//ChatGpt Response:
+// Sample response containing a timestamp
+// const response = {
+//   timestamp: "2023-08-02T12:34:56.789Z"
+// };
+
+// // Convert the timestamp to seconds
+// const timestampInSeconds = Math.floor(new Date(response.timestamp).getTime() / 1000);
+
+// console.log(timestampInSeconds); // Output: 1679542096
 
 
 describe("POST Add Task Missing Name and Description", () => {
@@ -19,35 +30,16 @@ describe("POST Add Task Missing Name and Description", () => {
           url: "http://localhost:8080/todo/addTask?name=&description=",  
           failOnStatusCode: false,}).then((response) => {
           expect(response.status).to.eq(400);
-          // createdTaskId = (response.body.taskId);
-          // cy.log("response.body.taskId: " + response.body.taskId);
+          const createdTimestamp = Math.floor(new Date(response.body.timestamp).getTime() / 1000);
+          const submissionTime = new Date().getTime()/1000;
+          expect(submissionTime - createdTimestamp).to.be.lessThan(5000);
+          // The below supplied automatically by GitHub CoPilot
+          if (response.body.error) {
+            expect(response.body.error).to.eq("Bad Request");
+          }
+          if (response.body.path) {
+            expect(response.body.path).to.eq("/todo/addTask?name=&description=");
+          }
         });
     });
-
-    it.skip("should return a response of 200 and the length of the response should increment by 1 after Task 4 has been added", () => {
-      cy.request('GET', 'http://localhost:8080/todo/').then((response) => {
-        expect(response.status).to.eq(200);
-        newLength = response.body.length;
-        cy.log("newLength: " + newLength);
-        expect(newLength).to.eq(initialLength + 1);
-        });
-      });
-
-    it.skip("should have a non-null or undefined UUID and all fields should contain the correct values", () => {
-      cy.request('GET', 'http://localhost:8080/todo/' + createdTaskId).then((response) => {
-      cy.log("createdTaskId: " + createdTaskId);
-      const task = response.body;
-      cy.log(response.body)
-      expect(task).to.not.be.null;
-      expect(task).to.not.be.undefined;
-      expect(task.name).to.eq(taskName);
-      expect(task.description).to.eq(taskDescription);
-      expect(task.created).to.not.be.null;
-      expect(task.completed).to.be.null;
-      expect(task.complete).to.eq(false);
-      const createdTimestamp = new Date(task.created).getTime();
-      const submissionTime = new Date().getTime();
-      expect(submissionTime - createdTimestamp).to.be.lessThan(5000);
-    });
-});
 });
