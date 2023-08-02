5f2c58b11aad158e8b9000c81ef4aae03a366952
Ged Smith <gsmith@scottlogic.com>
2023-08-02 16:53:10 +0100

    POST Task 4 completed

---
+++ test/cypress/e2e/api-tests/postToDoMissingName.cy.js
@@ -0,0 +1,22 @@
+//No GenAI used - simply copy/paste job from previous Task
+describe("POST Add Task Missing Name", () => {
+
+    it("should add Task with no name and return a response of 400", () => {
+        cy.request({
+          method: "POST", 
+          url: "http://localhost:8080/todo/addTask?name=&description=Task4Description",  
+          failOnStatusCode: false,}).then((response) => {
+          expect(response.status).to.eq(400);
+          const createdTimestamp = Math.floor(new Date(response.body.timestamp).getTime() / 1000);
+          const submissionTime = new Date().getTime()/1000;
+          expect(submissionTime - createdTimestamp).to.be.lessThan(5000);
+          // The below supplied automatically by GitHub CoPilot
+          if (response.body.error) {
+            expect(response.body.error).to.eq("Bad Request");
+          }
+          if (response.body.path) {
+            expect(response.body.path).to.eq("/todo/addTask?name=&description=Task4Description");
+          }
+        });
+    });
+    });
