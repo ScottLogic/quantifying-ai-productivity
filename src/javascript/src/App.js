2c998fbbf35fc95f937b0dc7cbcc66ea81b5fa6a
Matthew Griffin <mgriffin@scottlogic.com>
2023-08-18 12:15:25 +0100

    Post new task

--- src/javascript/src/App.js
+++ src/javascript/src/App.js
@@ -116,4 +116,47 @@ app.put('/todo/completed/:uuid', (req, res) => {
     }
 });
 
+// Add a new POST endpoint that takes two query parameters, task name and task description, that creates a new task item with the given name and description. The uuid of the new task will be assigned by the server as a random uuid and the created timestamp should be set to the current time. The new item will have no value for the completed timestamp and a value of false for the complete flag. The body of the response should include the uuid of the new task and a string message.
+app.post('/todo/addTask', (req, res) => {
+    const { name, description } = req.query;
+    // validate whether name and description are provided
+    if (!name || !description) {
+        const wasNameEmptyString = name === '';
+        const wasDescriptionEmptyString = description === '';
+
+        // html encode the name and description if they were defined - even an empty string
+        const encodedName = name || wasNameEmptyString ? encodeURIComponent(name) : undefined;
+        const encodedDescription = description || wasDescriptionEmptyString ? encodeURIComponent(description) : undefined;
+
+        // if not, return a 400 Bad Request with and object containing the error
+        // path should contain encoded query parameters only if they were provided
+        res.status(400).json({
+            timestamp: new Date().toISOString(),
+            status: 400,
+            error: 'Bad Request',
+            path: '/todo/addTask?' + (encodedName || wasNameEmptyString ? 'name=' + encodedName + (!encodedDescription && !wasDescriptionEmptyString ? '' : '&') : '') + (encodedDescription || wasDescriptionEmptyString ? 'description=' + encodedDescription : ''),
+        });
+        return;
+    }
+
+    // create a new task object
+    const newTask = {
+        uuid: require('uuid').v4(),
+        name,
+        description,
+        created: new Date().toISOString(),
+        completed: null,
+        complete: false
+    };
+
+    // add the new task to the tasks array
+    tasks.push(newTask);
+
+    // return 201 with object containing taskId and message with the name of the new task
+    res.status(201).json({
+        taskId: newTask.uuid,
+        message: 'Task ' + newTask.name + ' added successfully.', 
+    });
+});
+
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
