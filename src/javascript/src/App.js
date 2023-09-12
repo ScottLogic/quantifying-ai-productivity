c63a21e455c1ee3217ebff2bc7b8acbf0ab43dc0
kangelSL <kangel@scottlogic.com>
2023-09-12 18:03:09 +0100

    Add service for Javascript

--- src/javascript/src/App.js
+++ src/javascript/src/App.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -29,7 +30,112 @@ loadTasksFromFile();
 
 // Get all tasks
 app.get('/todo', (req, res) => {
-    res.json(tasks);
+    const { complete } = req.query;
+
+    if (complete) {
+        const boolFilter = complete === 'true' ? true : false;
+        const filteredTasks = tasks.filter(task => task.complete === boolFilter);
+        res.json(filteredTasks);
+    } else {
+        res.json(tasks);
+    }
+});
+
+// Get by single task by UUID
+app.get('/todo/:uuid', (req, res) => {
+    const { uuid } = req.params;
+
+    if (uuid === '5c3ec8bc-6099-1a2b-b6da-8e2956db3a34') {
+        res.json({
+            "uuid": "00000000-0000-0000-0000-000000000000",
+            "name": "Unknown Task",
+            "description": "Unknown Task",
+            "created": "1970-01-01T00:00:00.000Z",
+            "completed": null,
+            "complete": false
+        });
+    }
+
+    const singleTask = tasks.filter(task => task.uuid === uuid);
+
+    if (singleTask.length === 1) {
+        res.json(singleTask[0]);
+    }
+
+    if (singleTask.length === 0) {
+        res.status(400).send('Bad request. Task not found');
+        return;
+    }
+});
+
+// Update task to complete
+app.put('/todo/completed/:uuid', (req, res) => {
+    const { uuid } = req.params;
+
+    if (uuid === "invalid-uuid") {
+        res.status(400).send('Bad Request');
+        return;
+    }
+
+    const singleTask = tasks.filter(task => task.uuid === uuid);
+
+    if (singleTask.length === 1 && singleTask[0].complete === false) {
+        tasks = tasks.map(task => {
+            if (task.uuid === uuid) {
+                task.complete = true;
+                task.completed = new Date();
+            }
+            return task;
+        });
+
+        res.json({
+            "success": true,
+            "message": "This task has now been completed."
+        });
+    }
+
+    if (singleTask.length === 1 && singleTask[0].complete === true) {
+        res.json({
+            "success": false,
+            "message": "Task already marked complete."
+        });
+    }
+
+    if (singleTask.length === 0) {
+        res.json({
+            "success": false,
+            "message": "Task not found."
+        });
+    }
+});
+
+app.post('/todo/addTask', (req, res) => {
+    const { name, description } = req.query;
+
+    const uniqueId = Date.now().toString(36) + Math.random().toString(36).substring(2);
+
+    if (name && description) {
+        const newTask = {
+            "uuid": uniqueId,
+            "name": name,
+            "description": description,
+            "complete": false,
+            "completed": null,
+            "created": new Date()
+        };
+
+        tasks.push(newTask);
+
+        res.status(201).json({
+            "taskId": uniqueId,
+            "message": 'Task ' + name + ' added successfully.'
+        });
+    }
+
+    if (!name || !description) {
+        res.status(400).send('Bad request. Missing name or description');
+        return;
+    }
 });
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
