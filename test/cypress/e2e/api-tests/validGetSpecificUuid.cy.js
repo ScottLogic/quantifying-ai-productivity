7eced89bb3c526487869fe172798616258074709
lclark-scottlogic <lclark@scottlogic.com>
2023-09-13 10:02:27 +0100

    remove AI generated commenting

--- test/cypress/e2e/api-tests/validGetSpecificUuid.cy.js
+++ test/cypress/e2e/api-tests/validGetSpecificUuid.cy.js
@@ -1,17 +1,9 @@
 describe('Valid Get Task By UUID', () => {
     it('Valid UUID', () => {
-      // Replace 'valid-uuid-here' with an actual valid UUID from your data
       const validUUID = 'f360ba09-4682-448b-b32f-0a9e538502fa';
-  
-      // Make a GET request to the endpoint with the valid UUID
       cy.request('GET', `http://localhost:8080/todo/${validUUID}`).then((response) => {
-        // Check that the response status code is 200
         expect(response.status).to.equal(200);
-  
-        // Get the task from the response body
         const task = response.body;
-  
-        // Assert that the name and description match the expected values
         expect(task.name).to.equal('Walk the dog');
         expect(task.description).to.equal('Walk the dog for forty five minutes');
       });
