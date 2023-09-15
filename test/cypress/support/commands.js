abf69e5fa3d4066345abae5c93610236dfe6156a
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2023-09-15 14:05:33 +0100

    Alter command method to take PUT / POST

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -1,38 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('logApiRequest', (method, url, requestBody = null) => {
+  cy.request({
+    method: method,
+    url: url,
+    body: requestBody,
+  }).then((response) => {
+    // Log the number of items in the array
+    const arrayLength = response.body.length;
+    cy.log(`Received ${arrayLength} tasks from the API:`);
 
-Cypress.Commands.add('logApiResponse', (url) => {
-  cy.request('GET', url).then((response) => {
-    // Log the response with properties and values for each object in the array
+    // Log the properties of each object in the array
     response.body.forEach((item, index) => {
-      const arrayLength = response.body.length;
       const objectDetails = Object.entries(item)
         .map(([key, value]) => `${key}: ${value}`)
         .join(', ');
-      cy.log(`Received ${arrayLength} tasks from the API:`);
       cy.log(`Task ${index + 1}: { ${objectDetails} }`);
     });
   });
