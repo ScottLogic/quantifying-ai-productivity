1d95c48cee28f6a220fcea719a9c03a5f23a20ef
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2023-09-15 13:17:26 +0100

    Set up logging command

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -23,3 +23,17 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('logApiResponse', (url) => {
+  cy.request('GET', url).then((response) => {
+    // Log the response with properties and values for each object in the array
+    response.body.forEach((item, index) => {
+      const arrayLength = response.body.length;
+      const objectDetails = Object.entries(item)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join(', ');
+      cy.log(`Received ${arrayLength} tasks from the API:`);
+      cy.log(`Task ${index + 1}: { ${objectDetails} }`);
+    });
+  });
+});
