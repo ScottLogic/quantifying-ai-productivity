0d0f81273a5c704211be8997ead2adc3b068c725
vferraro-scottlogic <vferraro@scottlogic.com>
2023-09-20 15:52:56 +0100

    initial commit

--- src/javascript/src/App.js
+++ src/javascript/src/App.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4, validate } = require('uuid')
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,13 @@ app.use(express.json());
 const tasksFilePath = path.join(__dirname, '../../static_data', 'ToDoList.json');
 
 let tasks = [];
+
+const badRequestJSON = (url) => ({
+    "timestamp": new Date().toISOString(),
+    "status": 400,
+    "error": "Bad Request",
+    "path": url
+})
 
 // Load tasks from the JSON file
 const loadTasksFromFile = () => {
@@ -29,7 +37,105 @@ loadTasksFromFile();
 
 // Get all tasks
 app.get('/todo', (req, res) => {
-    res.json(tasks);
+    const { complete } = req.query;
+
+    let filteredTasks = tasks;
+
+    if (complete !== undefined) {
+        filteredTasks = filteredTasks.filter(task => task.complete.toString() === complete)
+    }
+
+    res.json(filteredTasks);
+});
+
+// Get task by uuid
+app.get('/todo/:uuid', (req, res) => {
+    const { uuid } = req.params;
+
+    const isValidUUID = validate(uuid)
+
+    if (!isValidUUID) {
+        res.status(400).send(badRequestJSON(req.url))
+        return
+    }
+
+    const foundTask = tasks.find(task => task.uuid === uuid)
+
+    if (!foundTask) {
+        res.json({
+            uuid: "00000000-0000-0000-0000-000000000000",
+            name: "Unknown Task",
+            description: "Unknown Task",
+            created: new Date(0).toISOString(),
+            completed: null,
+            complete: false
+        })
+    }
+    else {
+        res.json(foundTask);
+    }
+});
+
+// Mark task as complete
+app.put('/todo/completed/:uuid', (req, res) => {
+    const { uuid } = req.params;
+
+    const isValidUUID = validate(uuid)
+
+    if (!isValidUUID) {
+        res.status(400).send(badRequestJSON(req.url))
+        return
+    }
+
+    const foundTask = tasks.find(task => task.uuid === uuid)
+
+    if (!foundTask) {
+        res.json({
+            success: false,
+            message: "Task not found."
+        })
+        return
+    }
+    if (foundTask.complete) {
+        res.json({
+            success: false,
+            message: "Task already marked complete."
+        })
+        return
+    }
+
+    foundTask.complete = true;
+    foundTask.completed = new Date().toISOString();
+
+    res.json({
+        success: true,
+        message: "This task has now been completed."
+    });
+});
+
+// Create task
+app.post('/todo/addTask', (req, res) => {
+    const { name, description } = req.query;
+
+    const id = uuidv4()
+
+    if (!name || !description) {
+        res.status(400).send(badRequestJSON(req.url))
+        return
+    }
+
+    tasks.push({
+        name,
+        description,
+        uuid: id,
+        created: new Date().toISOString(),
+        complete: false
+    })
+
+    res.status(201).json({
+        id,
+        message: "Task " + name + " added successfully."
+    })
 });
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
