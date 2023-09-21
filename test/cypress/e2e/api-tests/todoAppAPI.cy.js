15c32e8ac9b8e71c67723e4aa4a6b3575afb9f84
Sara Jurics <sjurics@scottlogic.com>
2023-09-21 15:55:37 +0100

    GET Task4

--- test/cypress/e2e/api-tests/todoAppAPI.cy.js
+++ test/cypress/e2e/api-tests/todoAppAPI.cy.js
@@ -25,11 +25,23 @@ describe('Todo App API', () => {
         })
       })
     
-      it('should get all incompleted tasks', () => {
-        cy.request('http://localhost:8080/todo?complete=false').should((response) => {
-          expect(response.status).to.equal(200)
-          expect(response.body).to.be.an('array')
-          expect(response.body).to.have.length(3)
-          })
+    it('should get all incompleted tasks', () => {
+      cy.request('http://localhost:8080/todo?complete=false').should((response) => {
+        expect(response.status).to.equal(200)
+        expect(response.body).to.be.an('array')
+        expect(response.body).to.have.length(3)
         })
+      })
+
+    it('should get task number 3', () => {
+      // Define the UUID of the task you want to retrieve
+      const taskUuid = '5c3ec8bc-6099-4cd5-b6da-8e2956db3a34';
+  
+      cy.request(`http://localhost:8080/todo/${taskUuid}`).should((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('uuid', taskUuid);
+        expect(response.body).to.have.property('name', 'Test generative AI');
+        expect(response.body).to.have.property('description', 'Use generative AI technology to write a simple web service');
+      });
+    });
   })
