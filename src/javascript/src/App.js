69a05059c996423446742c307461001d153d3c48
dcottle-scottlogic <125259915+dcottle-scottlogic@users.noreply.github.com>
2023-12-04 11:45:51 +0000

    get endpoints added

--- src/javascript/src/App.js
+++ src/javascript/src/App.js
@@ -1,3 +1,4 @@
+const { timeStamp } = require('console');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -6,6 +7,8 @@ const app = express();
 app.use(express.json());
 
 const tasksFilePath = path.join(__dirname, '../../static_data', 'ToDoList.json');
+
+const uuidPattern = new RegExp('^([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$', 'i');
 
 let tasks = [];
 
@@ -29,7 +32,40 @@ loadTasksFromFile();
 
 // Get all tasks
 app.get('/todo', (req, res) => {
-    res.json(tasks);
+    if (req.query.complete == undefined){
+        res.json(tasks);
+    } else {
+        res.json(tasks.filter((task) => (task.complete).toString() == req.query.complete));
+    }
 });
+
+app.get('/todo/:uuid?', (req, res) => {
+    if (req.params.uuid == undefined || !uuidPattern.test(req.params.uuid)){
+        res.status(400).json({ 
+            timestamp: new Date().valueOf(),
+            message: "Bad Request", 
+            status: 400,
+            path: "/todo/invalid-uuid"
+          });
+    } else {
+        const requestedTask = tasks.filter((task) => task.uuid.toString() == req.params.uuid)
+        if (requestedTask.length == 0) {
+            res.status(200).json({
+                uuid: "00000000-0000-0000-0000-000000000000",
+                name: "Unknown Task",
+                description: "Unknown Task",
+                created: "1970-01-01T00:00:00.000Z",
+                completed: null,
+                complete: false
+            }
+
+            )
+        }else {
+            res.json(requestedTask)
+        }
+    }
+});
+
+
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
