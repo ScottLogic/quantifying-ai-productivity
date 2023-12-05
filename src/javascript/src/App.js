a337fa9f41f10bf88a436cd7ba7af90e8cd6a022
HélèneSauvé <hsauve@scottlogic.com>
2023-12-05 11:10:52 +0000

    Add a new PUT endpoint to mark a task as complete

--- src/javascript/src/App.js
+++ src/javascript/src/App.js
@@ -39,4 +39,92 @@ app.get('/todo', (req, res) => {
     res.json(filteredTasks);
 });
 
+const { v4: uuidv4 } = require('uuid'); // Import the uuid library for generating valid UUIDs
+
+app.get('/todo/:uuid', (req, res) => {
+    const requestedUuid = req.params.uuid;
+
+    // Validate the UUID format
+    const uuidRegex = /^[a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}$/i;
+    if (!uuidRegex.test(requestedUuid)) {
+        // Invalid UUID format
+        return res.status(400).json({
+            timestamp: new Date().toISOString(),
+            status: 400,
+            error: 'Bad Request',
+            path: req.path
+        });
+    }
+
+    // Find the task with the specified UUID
+    const task = tasks.find(task => task.uuid === requestedUuid);
+
+    if (task) {
+        res.json(task);
+    } else {
+        const unknownTask = {
+            uuid: "00000000-0000-0000-0000-000000000000",
+            name: "Unknown Task",
+            description: "Unknown Task",
+            created: "1970-01-01T00:00:00.000Z",
+            completed: null,
+            complete: false
+        };
+        res.json(unknownTask);
+    }
+});
+
+app.put('/todo/completed/:uuid', (req, res) => {
+    const requestedUuid = req.params.uuid;
+
+    // Validate the UUID format
+    const uuidRegex = /^[a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}$/i;
+    if (!uuidRegex.test(requestedUuid)) {
+        // Invalid UUID format
+        return res.status(400).json({
+            timestamp: new Date().toISOString(),
+            status: 400,
+            error: 'Bad Request',
+            path: req.path
+        });
+    }
+
+    // Find the task with the specified UUID
+    const taskIndex = tasks.findIndex(task => task.uuid === requestedUuid);
+
+    if (taskIndex !== -1) {
+        const task = tasks[taskIndex];
+
+        // Check if the task is already marked as complete
+        if (task.complete) {
+            return res.status(200).json({
+                success: false,
+                message: 'Task already marked complete.'
+            });
+        }
+
+        // Mark the task as complete
+        task.completed = new Date().toISOString();
+        task.complete = true;
+
+        // Update the task in the tasks array
+        tasks[taskIndex] = task;
+
+        // Respond with success message
+        return res.status(200).json({
+            success: true,
+            message: 'This task has now been completed.'
+        });
+    } else {
+        // Task not found
+        return res.status(200).json({
+            success: false,
+            message: 'Task not found.'
+        });
+    }
+});
+
+
+
+
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
