93ccbf1feea1e9dadefb3dec51ed58047a498054
Roger Sillito <rogersillito@gmail.com>
2023-12-06 11:20:50 +0000

    Revert "inline the tasks defn for chatgpt friendliness"
    This reverts commit da4f2e3f5f860739d6271f861df7e745998a3f7d.

--- src/javascript/src/App.js
+++ src/javascript/src/App.js
@@ -6,54 +6,31 @@ const { v4: uuidv4, validate: uuidValidate } = require("uuid");
 const app = express();
 app.use(express.json());
 
-// const tasksFilePath = path.join(__dirname, '../../static_data', 'ToDoList.json');
+const tasksFilePath = path.join(
+  __dirname,
+  "../../static_data",
+  "ToDoList.json"
+);
 
-// let tasks = [];
+let tasks = [];
 
-// // Load tasks from the JSON file
-// const loadTasksFromFile = () => {
-//     fs.readFile(tasksFilePath, 'utf8', (err, data) => {
-//         if (err) {
-//             console.error('Error reading tasks file:', err);
-//             return;
-//         }
-//         try {
-//             tasks = JSON.parse(data);
-//         } catch (error) {
-//             console.error('Error parsing tasks JSON:', error);
-//         }
-//     });
-// };
+// Load tasks from the JSON file
+const loadTasksFromFile = () => {
+  fs.readFile(tasksFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading tasks file:", err);
+      return;
+    }
+    try {
+      tasks = JSON.parse(data);
+    } catch (error) {
+      console.error("Error parsing tasks JSON:", error);
+    }
+  });
+};
 
-// // Load tasks from the file when the server starts
-// loadTasksFromFile();
-
-let tasks = [
-  {
-    uuid: "f360ba09-4682-448b-b32f-0a9e538502fa",
-    name: "Walk the dog",
-    description: "Walk the dog for forty five minutes",
-    created: "2023-06-23T09:30:00Z",
-    completed: null,
-    complete: false,
-  },
-  {
-    uuid: "fd5ff9df-f194-4c6e-966a-71b38f95e14f",
-    name: "Mow the lawn",
-    description: "Mow the lawn in the back garden",
-    created: "2023-06-23T09:00:00Z",
-    completed: null,
-    complete: false,
-  },
-  {
-    uuid: "5c3ec8bc-6099-4cd5-b6da-8e2956db3a34",
-    name: "Test generative AI",
-    description: "Use generative AI technology to write a simple web service",
-    created: "2023-06-23T09:00:00Z",
-    completed: null,
-    complete: false,
-  },
-];
+// Load tasks from the file when the server starts
+loadTasksFromFile();
 
 // Get all tasks or filter by completion status
 app.get("/todo", (req, res) => {
